7e705128f0d6873c12ae288d41dd776eb39b6ddd
Blossom <saphashb@gmail.com>
2022-10-05 12:28:28 +0100

    design footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faBars,
@@ -13,66 +13,66 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 
 const date = () => {
-  const date = new Date();
-  let year = date.getFullYear();
-  return year;
+	const date = new Date();
+	let year = date.getFullYear();
+	return year;
 };
 const Footer = () => {
+	return (
+		<div className="footer">
+			<div className="footer_inner">
+				<p className="footer_copyright">
+					Copyright &copy; <span>{date()} </span> Trant
+				</p>
 
-  return (
-    <div className='footer'>
-      <div className="footer_inner">
-      <div>
-      <p className="footer-body">
-				Copyright &copy; <span>{date()} </span> Trant
-			</p>
-      </div>
+				<div className='footer_social ctn'>
+					<ul className="footer_social">
+						<li>
+							<a
+								href="https://www.linkedin.com/in/blossom-babalola/"
+								target="_blank"
+								rel="noreferrer">
+								<FontAwesomeIcon icon={faLinkedin} />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://github.com/blossom-babs/"
+								target="_blank"
+								rel="noreferrer">
+								<FontAwesomeIcon icon={faGithub} />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://anchor.fm/trant"
+								target="_blank"
+								rel="noreferrer">
+								<FontAwesomeIcon icon={faMicrophone} />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.youtube.com/channel/UCWqoKQfyZTTLUd4t8yBT57g"
+								target="_blank"
+								rel="noreferrer">
+								<FontAwesomeIcon icon={faYoutube} />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.instagram.com/b_tranting/"
+								target="_blank"
+								rel="noreferrer">
+								<FontAwesomeIcon icon={faInstagram} />
+							</a>
+						</li>
+						{/* footer */}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-      <div>
-      <ul className="navbar-sm">
-					<li>
-						<a
-							href="https://www.linkedin.com/in/blossom-babalola/"
-							target="_blank"
-							rel="noreferrer">
-							<FontAwesomeIcon icon={faLinkedin} />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/blossom-babs/"
-							target="_blank"
-							rel="noreferrer">
-							<FontAwesomeIcon icon={faGithub} />
-						</a>
-					</li>
-					<li>
-						<a href="https://anchor.fm/trant" target="_blank" rel="noreferrer">
-							<FontAwesomeIcon icon={faMicrophone} />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://www.youtube.com/channel/UCWqoKQfyZTTLUd4t8yBT57g"
-							target="_blank"
-							rel="noreferrer">
-							<FontAwesomeIcon icon={faYoutube} />
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://www.instagram.com/b_tranting/"
-							target="_blank"
-							rel="noreferrer">
-							<FontAwesomeIcon icon={faInstagram} />
-						</a>
-					</li>
-					{/* footer */}
-				</ul>
-      </div>
-      </div>
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;
