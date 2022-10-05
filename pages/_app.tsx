bd32a5f9db1483567d2dcaf5b0ccbbb2ff99bf34
Blossom <saphashb@gmail.com>
2022-10-05 12:38:10 +0100

    style the app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
-import '../styles/index.scss'
-import type { AppProps } from 'next/app'
-import {Header, Footer} from '../components/'
+/** @format */
+
+import '../styles/index.scss';
+import type { AppProps } from 'next/app';
+import { Header, Footer } from '../components/';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className='app'>
-  <Header/>
-  <Component {...pageProps} />
-  <Footer/>
-  </div>
+	return (
+		<div className="app">
+			<Header />
+			<div className="app_component">
+				<Component {...pageProps} />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
