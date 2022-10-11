11edc7038254d75a95c6554e3cfea824a7d9c0f2
Blossom <saphashb@gmail.com>
2022-10-11 13:16:41 +0100

    create dynamic route for blog page

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import { NextPage } from 'next';
+import React from 'react';
+import { GetPostResults, Post } from '../../types';
 
-const BlogPost = () => {
-  return (
-    <div>BlogPost</div>
-  )
-}
+const BlogPost: NextPage<{ post: Post }> = ({ post }) => {
+	return (
+		<article>
+			<header>
+ 				<h1>{post.attributes.title}</h1>
+				<h2>{post.attributes.content}</h2>
+			</header>
+		</article>
+	);
+};
 
-export default BlogPost
+export const getStaticProps = async ({ params }: any) => {
+	const postsRes: any = await axios.get(
+		`http://localhost:1337/api/posts/${params.id}?populate=*`
+	);
+	const result: GetPostResults = postsRes.data.data;
+
+	return {
+		props: {
+			post: result
+		}
+	};
+};
+
+export const getStaticPaths = async () => {
+	const res: any = await axios.get(
+		'http://localhost:1337/api/posts?populate=*'
+	);
+	const posts: Post[] = res.data.data;
+	const paths = posts.map((post) => {
+		return { params: { id: post.id.toString() } };
+	});
+
+	return {
+		paths,
+		fallback: false
+	};
+};
+
+export default BlogPost;
